753ecbb3d84fe09651aa42e258f7a1a49cefb031
TK <118142360+tak40@users.noreply.github.com>
2024-07-03 10:50:15 -0400

    Add initial JavaScript functionality for image gallery navigation and refactor CSS for DRY principles
    - Implement JavaScript to handle 'next' button functionality:
      - Select elements for previous and next buttons, and gallery images.
      - Add event listener for 'next' button to navigate through images.
      - Update currently selected image and toggle 'active' class.
      - Disable 'next' button when the last image is reached.
    
    - Set up placeholder for 'prev' button functionality (to be implemented).
    - Refactor CSS to follow DRY principles, reducing redundancy and improving maintainability.

---
+++ js/gallery.js
@@ -0,0 +1,26 @@
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const galleryImages = document.querySelectorAll(".gallery-img");
+let currentlySelected = 0;
+
+// when you click prev button, the previous image will be shown but it should stop at the first image
+
+prevBtn.addEventListener("click", function () {
+  // make .active class to the previous image
+  // remove .active class from the current image
+});
+
+// when you click next button, the next image will be shown but it should stop at the last image
+
+nextBtn.addEventListener("click", function () {
+  // make .active class to the next
+  // remove .active class from the current image
+  galleryImages[currentlySelected].classList.remove("active");
+  currentlySelected++;
+  galleryImages[currentlySelected].classList.add("active");
+  prevBtn.disabled = false;
+
+  if (galleryImages.length === currentlySelected + 1) {
+    nextBtn.disabled = true;
+  }
+});
